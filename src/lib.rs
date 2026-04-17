8daade4fc8e70002b9b1bf69b84b7c12ed8cf8ec
fragilefort <imaddex3759@gmail.com>
2026-04-17 02:32:20 +0200

    Added the query construction module to convert user queries into JSON format

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,21 @@
-mod search;
+pub mod search;
+
+#[cfg(test)]
+mod tests {
+    use super::search::Query;
+
+    #[test]
+    fn test_query_serialization() {
+        let q = Query::And(vec![Query::Organism("Homo sapiens".into())]);
+        let json = serde_json::to_string_pretty(&q).unwrap();
+
+        assert!(json.contains("Homo sapiens"));
+        println!("{}", json);
+    }
+    #[test]
+    fn test_max_resolution_serialization() {
+        let q = Query::MaxResolution(2.5);
+        let json = serde_json::to_string_pretty(&q).unwrap();
+        println!("{}", json);
+    }
+}
--- src/search.rs
+++ src/search.rs
@@ -1,7 +1,113 @@
+use serde::ser::{Serialize, SerializeMap, Serializer};
+
 pub enum Query {
     Or(Vec<Query>),
     And(Vec<Query>),
     Organism(String),
     Keyword(String),
     MaxResolution(f32),
+}
+
+impl Serialize for Query {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            Query::And(queries) => {
+                let mut map = serializer.serialize_map(None)?;
+                map.serialize_entry("type", "group")?;
+                map.serialize_entry("logical_operator", "and")?;
+                map.serialize_entry("nodes", queries)?;
+                map.end()
+            }
+            Query::Or(queries) => {
+                let mut map = serializer.serialize_map(None)?;
+                map.serialize_entry("type", "group")?;
+                map.serialize_entry("logical_operator", "or")?;
+                map.serialize_entry("nodes", queries)?;
+                map.end()
+            }
+            Query::Organism(org) => {
+                let mut map = serializer.serialize_map(None)?;
+                map.serialize_entry("type", "terminal")?;
+                map.serialize_entry("service", "text")?;
+                map.serialize_entry(
+                    "parameters",
+                    &ParameterMap {
+                        attribute: "rcsb_entity_source_organism.taxonomy_lineage.name",
+                        operator: "exact_match",
+                        value: ParameterValue::Str(org),
+                    },
+                )?;
+                map.end()
+            }
+
+            Query::Keyword(kw) => {
+                let mut map = serializer.serialize_map(None)?;
+                map.serialize_entry("type", "terminal")?;
+                map.serialize_entry("service", "text")?;
+                map.serialize_entry(
+                    "parameters",
+                    &ParameterMap {
+                        attribute: "struct_keywords.pdbx_keywords",
+                        operator: "contains_phrase",
+                        value: ParameterValue::Str(kw),
+                    },
+                )?;
+                map.end()
+            }
+
+            Query::MaxResolution(mr) => {
+                let mut map = serializer.serialize_map(None)?;
+                map.serialize_entry("type", "terminal")?;
+                map.serialize_entry("service", "text")?;
+                map.serialize_entry(
+                    "parameters",
+                    &ParameterMap {
+                        attribute: "refine.ls_d_res_high",
+                        operator: "less_or_equal",
+                        value: ParameterValue::Float(*mr),
+                    },
+                )?;
+                map.end()
+            }
+        }
+    }
+}
+
+enum ParameterValue<'a> {
+    Str(&'a str),
+    Float(f32),
+}
+
+impl<'a> Serialize for ParameterValue<'a> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            ParameterValue::Str(s) => serializer.serialize_str(s),
+            ParameterValue::Float(f) => serializer.serialize_f32(*f),
+        }
+    }
+}
+
+struct ParameterMap<'a> {
+    attribute: &'a str,
+    operator: &'a str,
+    value: ParameterValue<'a>,
+}
+
+impl<'a> Serialize for ParameterMap<'a> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut map = serializer.serialize_map(None)?;
+        map.serialize_entry("attribute", self.attribute)?;
+        map.serialize_entry("operator", self.operator)?;
+        map.serialize_entry("value", &self.value)?;
+        map.end()
+    }
 }
